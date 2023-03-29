daaafc19dc2560d8b39ef67ccfb0150705605086
sourabhpatel073 <sourabhpatel073@email.com>
2023-03-29 20:07:10 +0530

    day 2

--- src/App.js
+++ src/App.js
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+{/*  */}
--- src/Pages/Payments.jsx
+++ src/Pages/Payments.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { useState } from 'react';
+import AddressForm from './Payment/AddressForm';
+import PaymentForm from './Payment/PaymentForm'
+import Checkout from './Payment/Checkout';
+import Review from './Payment/Review';
+import Poster from "./Payment/right.png"
+import { Box } from '@mui/system';
 const Payments = () => {
+ 
+  
+
   return (
-    <div>Payments</div>
-  )
+    <div className="container">
+     <Checkout/> 
+    </div>
+  );
 }
 
 export default Payments
