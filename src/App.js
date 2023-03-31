be9a26f7d6aec1353e9f012f95c7db91559cf2d3
DishaGup <guptamansi201@gmail.com>
2023-03-31 18:16:55 +0530

    resolve error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-
-import Homepage from "./Pages/Homepage";
-import Navigationbar from "./Components/HomePage/Navigationbar";
-
-
-import AllRoutes from "./Routes/AllRoutes";
-import Banner from "./Components/HomePage/Banner";
-
+import { Wishlist } from "./Pages/Wishlist";
 
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
