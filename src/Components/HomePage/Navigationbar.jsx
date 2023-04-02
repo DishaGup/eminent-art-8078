72623f1385076b9f35ffab693e4802fae12767ee
RahulDudka <rahuldudka27@gmail.com>
2023-04-02 16:03:50 +0530

    some err fixed

--- src/Components/HomePage/Navigationbar.jsx
+++ src/Components/HomePage/Navigationbar.jsx
@@ -1,6 +1,15 @@
 import {
-  Box, Center, HStack, Editable,
-  EditableInput, EditablePreview, Flex, Image, Icon, Button,  Menu,
+  Box,
+  Center,
+  HStack,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Flex,
+  Image,
+  Icon,
+  Button,
+  Menu,
   MenuButton,
   MenuList,
   MenuItem,
@@ -8,84 +17,118 @@ import {
   MenuGroup,
   MenuOptionGroup,
   MenuDivider,
-
-} from '@chakra-ui/react'
-import React from 'react'
-import trendify_logo from '../../Assests/trendifyLogo.jpeg'
-import { CiLocationOn } from 'react-icons/ci'
-import { BsBell, BsHeart, BsCart3, BsSearch } from 'react-icons/bs'
-import "./Navigationabar.module.css"
-import { NavLink,Link } from 'react-router-dom'
+} from "@chakra-ui/react";
+import React from "react";
+import trendify_logo from "../../Assests/trendifyLogo.jpeg";
+import { CiLocationOn } from "react-icons/ci";
+import { BsBell, BsHeart, BsCart3, BsSearch } from "react-icons/bs";
+// import "./Navigationabar.module.css"
+import { NavLink, Link } from "react-router-dom";
 
 import { TbLogin } from "react-icons/tb";
-import NavDrop from './NavDrop'
+import NavDrop from "./NavDrop";
 const Navigationbar = () => {
   return (
-    <Box position='fixed' top='0' left='0'display={{base:'none',md:'block'}} right='0' p={3} w='100%' overflow='hidden' m={'auto'} zIndex='1000' backgroundColor='#fff'>
-      <Flex   justify='space-around'>
-        <Box w='20%' mx='20px'>
-          <Center w='80%' >
+    <Box
+      position="fixed"
+      top="0"
+      left="0"
+      display={{ base: "none", md: "block" }}
+      right="0"
+      p={3}
+      w="100%"
+      overflow="hidden"
+      m={"auto"}
+      zIndex="1000"
+      backgroundColor="#fff"
+    >
+      <Flex justify="space-around">
+        <Box w="20%" mx="20px">
+          <Center w="80%">
             <button>
               <NavLink to="/">
-
                 <Image src={trendify_logo} />
               </NavLink>
             </button>
           </Center>
         </Box>
-        <HStack h='2cm' role='search' w='60%' display='inline-flex' >
-          <Icon cursor='pointer' w='20px' h='20px' width='20px' ml='5px' as={BsSearch} backgroundColor='#e9f6f7' />
-          <Editable align='left' placeholder='What is on Your mind today?' p={1} w='70%' backgroundColor='#e9f6f7' color='#353535' fontWeight={'400'} border='1px solid #dbf0f2'>
-
-            <EditablePreview mx='20px' border='none' _placeholder={{ color: 'gray.200', fontWeight: '400' }} />
-            <EditableInput pl='20px' border='0px' textDecoration='none' />
+        <HStack h="2cm" role="search" w="60%" display="inline-flex">
+          <Icon
+            cursor="pointer"
+            w="20px"
+            h="20px"
+            width="20px"
+            ml="5px"
+            as={BsSearch}
+            backgroundColor="#e9f6f7"
+          />
+          <Editable
+            align="left"
+            placeholder="What is on Your mind today?"
+            p={1}
+            w="70%"
+            backgroundColor="#e9f6f7"
+            color="#353535"
+            fontWeight={"400"}
+            border="1px solid #dbf0f2"
+          >
+            <EditablePreview
+              mx="20px"
+              border="none"
+              _placeholder={{ color: "gray.200", fontWeight: "400" }}
+            />
+            <EditableInput pl="20px" border="0px" textDecoration="none" />
           </Editable>
-          <Button borderRadius='0' width='100px' h='auto 40px' lineHeight='14px' p='13px 25px' cursor='pointer' backgroundImage='-webkit-linear-gradient(0deg,#ff934b 0%,#ff5e62 100%)'>Search</Button>
+          <Button
+            borderRadius="0"
+            width="100px"
+            h="auto 40px"
+            lineHeight="14px"
+            p="13px 25px"
+            cursor="pointer"
+            backgroundImage="-webkit-linear-gradient(0deg,#ff934b 0%,#ff5e62 100%)"
+          >
+            Search
+          </Button>
         </HStack>
 
-        <HStack color='#24a3b5' fontSize={22} justify='space-between' w='15%' >
+        <HStack color="#24a3b5" fontSize={22} justify="space-between" w="15%">
           <Icon as={CiLocationOn} />
           <Icon as={BsBell} />
-        
-        
-          <Link to ='/wishlistpage' >  <Icon as={BsHeart} />  </Link>
 
-          <Link to ='/cartpage' >  <Icon as={BsCart3} />   </Link>
+          <Link to="/wishlistpage">
+            {" "}
+            <Icon as={BsHeart} />{" "}
+          </Link>
 
-
-         
-
+          <Link to="/cartpage">
+            {" "}
+            <Icon as={BsCart3} />{" "}
+          </Link>
 
           <Menu zIndex={200}>
-  <MenuButton as={Button} bg='white' size='20px' rightIcon={<TbLogin />}>
-  </MenuButton>
-  <MenuList fontSize='14px'>
-    <MenuItem >
-    <NavLink to='/login'>
-LOGIN/SIGNUP 
-</NavLink>
-    </MenuItem>
-    <MenuDivider m={0} />
-    <NavLink to='/adminpage'>
-    <MenuItem >
- SELL WITH US
-    </MenuItem>
-    </NavLink>
-  </MenuList>
-</Menu>
-        
-   </HStack>
-  <Box w='3%'>
-          {/* username */}
-        </Box>
+            <MenuButton
+              as={Button}
+              bg="white"
+              size="20px"
+              rightIcon={<TbLogin />}
+            ></MenuButton>
+            <MenuList fontSize="14px">
+              <MenuItem>
+                <NavLink to="/login">LOGIN/SIGNUP</NavLink>
+              </MenuItem>
+              <MenuDivider m={0} />
+              <NavLink to="/adminpage">
+                <MenuItem>SELL WITH US</MenuItem>
+              </NavLink>
+            </MenuList>
+          </Menu>
+        </HStack>
+        <Box w="3%">{/* username */}</Box>
       </Flex>
-      <NavDrop/>
-    
-    </Box >
-  )
-}
+      <NavDrop />
+    </Box>
+  );
+};
 
-
-
-
-export default Navigationbar
+export default Navigationbar;
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,16 +1,15 @@
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage";
 import NotFoundPage from "../Pages/NotFoundPage";
-import Registration from "../Pages/Registration";
+// import Registration from "../Pages/Registration";
 import { AdminPage } from "../Pages/Admin/AdminPage";
 import { AdminProducts } from "../Pages/Admin/AdminProducts/AdminProducts";
 import { AdminEditProduct } from "../Pages/Admin/AdminEditProduct";
 
-import { SingleProductPage } from '../Pages/SingleProductPage'
+import { SingleProductPage } from "../Pages/SingleProductPage";
 
-import Payments from './../Pages/Payments';
+import Payments from "./../Pages/Payments";
 import { Home } from "../Pages/Home";
 import ProductPage from "../Pages/ProductPage";
 import SingleProductPageMain from "../Components/ProductsPage/SingleProductPageMain";
@@ -18,28 +17,26 @@ import CartDisplayProduct from "../Pages/cart-display-product/CartDisplayProduct
 import { Wishlist } from "../Pages/Wishlist";
 import CartPage from "../Pages/cart/CartPage";
 
-
 const AllRoutes = () => {
-
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
-       <Route path="/cartpage" element={<CartPage/>  } /> 
-       <Route path="/wishlistpage" element={<Wishlist/>  } />
+      <Route path="/cartpage" element={<CartPage />} />
+      <Route path="/wishlistpage" element={<Wishlist />} />
       <Route path="/adminpage" element={<AdminPage />} />
       <Route path="/adminproducts" element={<AdminProducts />} />
       <Route path="/adminEditproducts/:id" element={<AdminEditProduct />} />
-      
-      <Route path="*" element={<NotFoundPage />} />      
-<Route path='/payments' element={ <Payments/>} />
-<Route path="/products/:category?"  element={<ProductPage/>}   />
-<Route  path="/products/:category?/:id/single?"  element={<SingleProductPageMain/> }  />
 
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/payments" element={<Payments />} />
+      <Route path="/products/:category?" element={<ProductPage />} />
+      <Route
+        path="/products/:category?/:id/single?"
+        element={<SingleProductPageMain />}
+      />
     </Routes>
   );
 };
-
-
 
 export default AllRoutes;
