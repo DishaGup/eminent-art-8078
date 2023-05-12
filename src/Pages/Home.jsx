b4a7cdc33c352eb55ad0acb9396524ec2ad87957
Sourabh Patel <sourabhpatel073@gmail.com>
2023-05-12 09:49:00 +0530

    Navbar on Wishlist is implimented

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -296,9 +296,8 @@ const DIV = styled.div`
 `;
 
 const BOX = styled.div`
-     background-colour: #fff
-     border='1px solid red'
-     
+     background-color: #fff
+     border='1px solid red';
 .imgtag:hover{
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
    
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,9 +1,12 @@
 import { Box, Heading } from "@chakra-ui/react";
 import LeftDiv from "./Wishlist/LeftDiv";
 import { RightDiv } from "./Wishlist/RightDiv";
+import Navmain from "../Components/HomePage/Navmain";
 
 const Wishlist = () => {
   return (
+    <>
+    <Navmain/>
     <Box h="100vh" w="98%" m="auto">
       <Box
         display={"flex"}
@@ -15,6 +18,7 @@ const Wishlist = () => {
         <RightDiv />
       </Box>
     </Box>
+    </>
   );
 };
 
