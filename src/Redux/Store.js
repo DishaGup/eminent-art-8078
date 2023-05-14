926eea4af40ae8bad49e702c5ef09f8bbf987fc6
RahulDudka <rahuldudka27@gmail.com>
2023-05-14 15:33:11 +0530

    redux part of cartpage

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,11 +2,14 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { reducer as adminReducer } from "./AdminReducer/reducer";
 import thunk from "redux-thunk";
 
-import {reducer as ProductReducer} from './ProductReducer/reducer'
-import {reducer as PaymentReducer} from './PaymentReducer/reducer'
-import {reducer as WishlistReducer} from "./WishList/reducer"
-const rootReducer=combineReducers({
-    ProductReducer, adminReducer,PaymentReducer,WishlistReducer
-})
+import { reducer as ProductReducer } from "./ProductReducer/reducer";
+import { reducer as PaymentReducer } from "./PaymentReducer/reducer";
+import { reducer as WishlistReducer } from "./WishList/reducer";
+const rootReducer = combineReducers({
+  ProductReducer,
+  adminReducer,
+  PaymentReducer,
+  WishlistReducer,
+});
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
